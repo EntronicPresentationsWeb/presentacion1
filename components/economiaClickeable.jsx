23b7ef9c33cd8bb113bfe4886b6af6eb4fb32e55
Entronic Presentations <gperez@entronic.mx>
2022-11-29 18:14:06 -0600

    pantalla 2 casi terminada

--- components/economiaClickeable.jsx
+++ components/economiaClickeable.jsx
@@ -26,6 +26,72 @@ export default function EconomiaClickeable() {
                     </Col>
                 </Row>
             </Container>
+            <Container>
+                <Row className="bg-img-row2-presentation1">
+                    <Col>
+                        <h4 className="color-white">Ven por la herramienta  <br />
+                            Quedate por la Red
+                        </h4>
+                    </Col>
+                    <Col>
+                        <Image src="/economiaClickeable/Numero-de-pagina2.png" width={343} height={150} className="float-right" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Row>
+                            <Image src="/economiaClickeable/LogoArcalia.png" width={258} height={67} />
+                        </Row>
+                        <Row>
+                            <Image src="/economiaClickeable/LogoFixup.png" width={200} height={136} />
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <div className="card">
+                                <Image src="/economiaClickeable/Icono-gestor-tareas.png" width={65} height={65} />
+                                <div className="card-padding">
+                                    <h4>Gestor de tareas</h4>
+                                </div>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className="card">
+                                <Image src="/economiaClickeable/Icono-administracion.png" width={65} height={65} />
+                                <div className="card-padding">
+                                    <h4>Administración y Gestión
+                                        de procesos para prestadores
+                                        de servicios tecnicos </h4>
+                                </div>
+                            </div>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <div>
+                                <Image src="/economiaClickeable/Flecha.png" width={108} height={50} />
+                            </div>
+                        </Row>
+                        <Row>
+                            <div>
+                                <Image src="/economiaClickeable/Flecha.png" width={108} height={50} />
+                            </div>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <div className="card">
+                                <Image src="/economiaClickeable/Icono-ecosistema.png" width={65} height={65} />
+                                <div className="card-padding">
+                                    <h4>Ecosistema Arcalia</h4>
+                                    <p>Demanda de servicios</p>
+                                    <p>Oferta servicios técnicos</p>
+                                </div>
+                            </div>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
